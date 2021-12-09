03823f843e892f45441e68be45b9fafd4b01b06f
Benjamin Idewor <benjaminidewor@gmail.com>
2021-12-09 17:50:43 +0100

    feat: add retreive account balance feature

--- routes/accounts.js
+++ routes/accounts.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const { auth } = require('../middlewares/auth');
-const { createAccountValidation, depositValidation } = require('../middlewares/accountsValidators');
-const { createAccount, deposit } = require('../controllers/accounts');
+const {
+    createAccountValidation,
+    depositValidation
+} = require('../middlewares/accountsValidators');
+const {
+    createAccount,
+    deposit,
+    getBalance
+} = require('../controllers/accounts');
 const router = express.Router();
 
 // create a new bank account
 router.post('/', [auth, createAccountValidation], createAccount);
 router.post('/:accountId/deposit', [auth, depositValidation], deposit);
-
+router.get('/:accountId/balance', auth, getBalance);
 module.exports = router;
